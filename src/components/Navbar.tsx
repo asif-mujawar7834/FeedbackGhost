ab3f57bf106ac9653b52351a2e80fcb4177c9f15
Asif Mujawar <asiflpnmujawar@gmail.com>
2024-06-12 13:20:24 +0530

    favicon added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
   return (
     <nav className=" bg-[#EEEEEE] sticky top-0 z-10 shadow-lg">
       <div className="max-w-[1100px] mx-auto p-7 flex justify-between items-center">
-        <h1 className="font-extrabold text-2xl sm:text-3xl text-black">
+        <h1 className="font-extrabold text-xl sm:text-3xl text-black">
           FeedbackGhost
         </h1>
 
